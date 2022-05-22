9e2bf6ea4696e4b13583a9b80e5068f58b047831
satsumaimoamo <meiabao25@gmail.com>
2022-05-23 00:05:56 +0900

    選管・神の投票先可視化をSNR対応

---
+++ SuperNewRoles/Patch/ViewVote.cs
@@ -0,0 +1,81 @@
+﻿using HarmonyLib;
+using Hazel;
+using SuperNewRoles.CustomOption;
+using SuperNewRoles.CustomRPC;
+using SuperNewRoles.Roles;
+using SuperNewRoles.Mode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperNewRoles.Patch
+{
+    public static class ViewVote
+    {
+        public static GameOptionsData OptionData;
+        public static void ViewVoteSettings(this PlayerControl player)
+        {
+            if (!AmongUsClient.Instance.AmHost) return;
+            if (!ModeHandler.isMode(ModeId.Default)) return;
+            var role = player.getRole();
+            var optdata = OptionData.DeepCopy2();
+            switch (role)
+            {
+                case RoleId.God:
+                    optdata.AnonymousVotes = !RoleClass.God.IsVoteView;
+                    break;
+                case RoleId.Observer:
+                    optdata.AnonymousVotes = !RoleClass.Observer.IsVoteView;
+                    break;
+                case RoleId.SubObserver:
+                    optdata.AnonymousVotes = !RoleClass.SubObserver.IsVoteView;
+                    break;
+            }
+            if (player.isDead()) optdata.AnonymousVotes = false;
+            optdata.RoleOptions.ShapeshifterLeaveSkin = false;
+            if (player.AmOwner) PlayerControl.GameOptions = optdata;
+            MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)RpcCalls.SyncSettings, SendOption.Reliable, player.getClientId());
+            writer.WriteBytesAndSize(optdata.ToBytes(5));
+            AmongUsClient.Instance.FinishRpcImmediately(writer);
+        }
+        public static float KillCoolSet(float cool)
+        {
+            if (cool <= 0)
+            {
+                return 0.001f;
+            } else
+            {
+                return cool;
+            }
+        }
+
+        public static void ViewVoteSettings()
+        {
+            foreach (PlayerControl p in PlayerControl.AllPlayerControls)
+            {
+                if (!p.Data.Disconnected && p.IsPlayer())
+                {
+                    ViewVoteSettings(p);
+                }
+            }
+        }
+        public static GameOptionsData DeepCopy2(this GameOptionsData opt)
+        {
+            var optByte = opt.ToBytes(5);
+            return GameOptionsData.FromBytes(optByte);
+        }
+        [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.StartGame))]
+        public class StartGame
+        {
+            public static void Prefix()
+            {
+             //   BotHandler.CreateBot();
+            }
+            public static void Postfix()
+            {
+                if (!AmongUsClient.Instance.AmHost) return;
+                OptionData = PlayerControl.GameOptions.DeepCopy2();
+            }
+        }
+    }
+}
